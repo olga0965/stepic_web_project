0647895d2e7536e27f8c3c5a285876a29051addb
olga0965 <kazakovaolga0909@gmail.com>
2020-02-12 20:02:37 +0000

    create folder

--- init_r.sh
+++ init_r.sh
@@ -1,5 +1,5 @@
-sudo ﻿ln -s /home/pi/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#!/bin/bash
+sudo ﻿ln -s /home/pi/web/etc/nginx/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/pi/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+#sudo ln -s /home/pi/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart﻿
